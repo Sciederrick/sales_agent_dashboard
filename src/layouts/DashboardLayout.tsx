958b92eefa0e6099f76d8cb478634341b2ba44c7
Sciederrick <derrickmbarani@gmail.com>
2024-06-09 02:06:43 +0300

    feat: add transition effects on the profile overlay for good UX

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -3,15 +3,13 @@ import { useContext } from "react";
 
 import { AppContext } from "../contexts/AppContext";
 
-import {
-    MdOutlineDashboard,
-    MdOutlineSchool,
-} from "react-icons/md";
+import { MdOutlineDashboard, MdOutlineSchool } from "react-icons/md";
 
 import logoIcon1 from "./../assets/logo-1.png";
 import logoIcon2 from "./../assets/logo-2.png";
 
 import Profile from "../components/Profile";
+import { Transition } from "@headlessui/react";
 
 type TypeLayoutProps = {
     children: React.ReactNode;
@@ -91,16 +89,28 @@ const DashboardLayout: React.FC<TypeLayoutProps> = ({ children }) => {
                     </li>
                 </ul>
             </nav>
-            <main className="min-h-screen bg-[#F6F6F6] w-full lg:pl-[250px]">{children}</main>
-            <aside
-                className={`z-10 min-h-screen absolute inset-0 bg-[#EAF6FC] ${
-                    ctx?.showProfileSideBar
-                        ? "block lg:w-[400px] lg:fixed lg:inset-auto lg:right-0 lg:inset-y-0 lg:shadow"
-                        : "hidden"
-                }`}
+            <main className="min-h-screen bg-[#F6F6F6] w-full lg:pl-[250px]">
+                {children}
+            </main>
+            <Transition
+                show={ctx?.showProfileSideBar}
+                enter="transition-transform duration-300"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition-transform duration-300"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
             >
-                <Profile />
-            </aside>
+                <aside
+                    className={`z-10 min-h-screen absolute inset-0 bg-[#EAF6FC] ${
+                        ctx?.showProfileSideBar
+                            ? "block lg:w-[400px] lg:fixed lg:inset-auto lg:right-0 lg:inset-y-0 lg:shadow"
+                            : "hidden lg:w-0"
+                    }`}
+                >
+                    <Profile />
+                </aside>
+            </Transition>
         </div>
     );
 };
